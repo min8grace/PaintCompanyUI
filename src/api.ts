bae3512332d0e1ff8a02e60e569cc399956c8d1e
min8grace <56316476+min8grace@users.noreply.github.com>
2024-03-28 00:30:05 -0700

    api update

--- src/api.ts
+++ src/api.ts
@@ -6,8 +6,7 @@ const instance = axios.create({
     baseURL:
         process.env.NODE_ENV === "development" ?
             "https://localhost:5001/" :
-            "https://backend.gracewaychurch.xyz/",
-    withCredentials: true,
+            "https://backend.gracewaychurch.xyz/"
 })
 
 
